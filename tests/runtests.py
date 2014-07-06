7b9b3560dd690250540bfc52ffe3d321bd471adc
Joe Russ Bowman <bowman.joseph@gmail.com>
2014-07-06 16:09:34 -0400

    starting to write the tests

---
+++ tests/runtests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandle):
+    """ Just a helloworld handler taken from the offical tornado docs to act
+    as a placeholder while I start to write tests."""
+    def get(self):
+        self.write("Hello, world")
+
+def main():
+    """ The tests module should start a web server capable of providing acting
+    as an oauth server. Since oauth depends on end points the oauth modules
+    should add handlers to the application. 
+
+    Included should be hooks for authentication, but it should be written to
+    support various forms of authentication.
+    
+    The tests module should be written to be able to handle any type of server
+    so this may all be rewritten to either stop and create new servers or to
+    edit the application on the fly. Haven't sorted that out yet."""
+
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+        ])
+# tornadoauth.addOauth2handlers(application)
+# This should test adding the handlers to the application
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+
