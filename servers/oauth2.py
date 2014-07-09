639ccd57dd764061fbfce4f9d0a11f3304f5ef8e
Joe Russ Bowman <bowman.joseph@gmail.com>
2014-07-08 21:37:17 -0400

    stopping for the night, may work on this on another computer so committing now. I'm starting to get what's needed. I'm working on setting up the oauth2 server now. Right now I'm working on the extract_params function which is how the django and flask modules provide uri, method, body (which I believe is a filled out form) and headers. I'm trying to model it after how flask and django do it because they are similar, might as well keep the tornado implementation as similar as possible as well.

---
+++ servers/oauth2.py
@@ -0,0 +1,37 @@
+# codeing: utf-8
+import tornado.web
+from oauthlib.oauth2 import WebApplicationServer
+
+class Oauth2Handler(tornado.web.RequestHandler):
+    def get_current_user():
+        pass
+        # stub, will return oauth identiier
+
+class Oauth2Server(object):
+    """ Oauth2Server modified the passed application object with everything
+    required to act as an Oauth2 server. It will add all the necessary
+    handlers and routes to the application.
+    
+    You also need to create your own requestValidator as defined in the
+    oauthlib docs:
+        http://oauthlib.readthedocs.org/en/latest/oauth2/validator.html
+    """
+
+    def __init__(self, application=None validator=None, server=None):
+        if not application:
+            raise RuntiumeError('Must pass an instance of tornado.application')
+        else:
+            self.application = application
+        if not validator:
+            raise RuntiumeError('Must pass a validator')
+        if not server:
+            server = WebApplicationServer
+        application.oauth2Server = server(validator)
+
+class AuthorizationViewHandler(tornado.web.requestHandler):
+    def get(self):
+
+
+            
+    
+
